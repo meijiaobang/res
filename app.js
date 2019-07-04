9730024cfd765517a13a1eff472fcb85365d5c5f
tan <15856027@qq.com>
2019-07-04 19:03:06 +0800

    three

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const routerDate=require("./router/index_data");
 const routerLogin=require("./router/login");
 const routerreg=require("./router/register");
 const routerprod=require("./router/product");
+
 let app=express();
+//托管静态资源到public目录
+app.use( express.static('public'));
 let server=http.createServer(app);
 server.listen(3000);
 console.log("服务器启动完成");
